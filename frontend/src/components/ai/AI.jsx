530f4b34dafc8394c6358c59c7dab5cc62588619
valentino0405 <gomesvalentino742@gmail.com>
2025-03-12 19:34:31 +0530

    DashBoard making

--- frontend/src/components/ai/AI.jsx
+++ frontend/src/components/ai/AI.jsx
@@ -1,9 +1,12 @@
 import Header from "./Header";
-
+import HealthDashboard from "./HealthDashboard";
 const AI = () => {
   return (
     <div>
             <Header/>
+            <div className='mx-5 md:mx-20 lg:mx-36'>
+            <HealthDashboard/>
+            </div>
     </div>
   );
 }
---
+++ frontend/src/components/ai/AddQuery.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AddQuery = () => {
+  return (
+    <div>
+      <div div className='p-10 border rounded-lg bg-secondary
+      hover:scale-105 hover:shadow-md cursor-pointer transition-all'>
+            <h2 className='font-bold text-lg text-center'>+Add New</h2>
+
+      </div>
+    </div>
+  );
+}
+
+export default AddQuery;
